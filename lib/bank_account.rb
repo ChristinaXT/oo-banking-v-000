6893a106f16976ced860d696824068c1fe6c32cc
Christina Williams <christinaxtwilliams@gmail.com>
2018-10-15 18:06:46 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,5 +2,10 @@ class BankAccount
   attr_accessor :balance, :status
 	  attr_reader :name
 	  
+	  def initialize(account_name)
+	    @name = account_name
+	    @balance = 1000
+	    @status = "open"
+	  end
 
 end
