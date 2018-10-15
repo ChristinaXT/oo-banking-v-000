83e676d76caa917842eeb037a99b99c3b68d1f59
Christina Williams <christinaxtwilliams@gmail.com>
2018-10-15 18:05:47 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,12 @@
+require 'pry'
 class Transfer
-  
+  attr_accessor :sender, :receiver, :amount, :status
+	
+
+	  def initialize(sender, receiver, amount)
+	    @sender = sender
+	    @receiver = receiver
+	    @amount = amount
+	    @status = "pending"
   
 end
